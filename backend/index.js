7de3b62d4fb2fd374abc88f402e9bb7e28bec18b
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-10 21:41:16 +0200

    get post put routes done

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 // with express.json() middleware
 
 const express = require("express");
-const { createTodo } = require("./types");
+const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
 
 const app = express();
@@ -29,11 +29,18 @@ app.get("/todos", async function (req, res) {
   res.json({ todos });
 });
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
-  const parsedPayload = createTodo.safeParse(updatePayload);
+  const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
     res.status(411).json({ message: "You sent the wrong inputs" });
     return;
   }
+  await todo.update(
+    { _id: req.body.id },
+    {
+      completed: true,
+    }
+  );
+  res.json({ message: "Todo marked as completed" });
 });
