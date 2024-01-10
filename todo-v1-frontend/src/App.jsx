4afb2f3135f7b3e990a6f8c7d62866a10972908d
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-10 23:47:31 +0200

    introduced todos state

--- todo-v1-frontend/src/App.jsx
+++ todo-v1-frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import { CreateTodo } from "./components/CreateTodo";
+import { Todos } from "./components/Todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  // fetch("http://localhost:3000/todos").then(async function (todos) {
+  //   const data = await todos.json();
+  //   setTodos(data);
+  // });
   return (
     <>
-      <div>hii there</div>
+      <div>
+        <CreateTodo />
+        <Todos todos={todos} />
+      </div>
     </>
   );
 }
