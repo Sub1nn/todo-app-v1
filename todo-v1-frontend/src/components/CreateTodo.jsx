26e24cd9f74185df0ff16d63301dcfe9d460437a
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-11 21:31:04 +0200

    CreateTodo component (raw) completed

--- todo-v1-frontend/src/components/CreateTodo.jsx
+++ todo-v1-frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+
 export function CreateTodo() {
+  // local state
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   return (
     <div>
-      <input type="text" placeholder="title" /> <br />
-      <input type="text" placeholder="description" /> <br />
-      <button>Add a todo</button>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={function (e) {
+          setTitle(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="description"
+        onChange={function (e) {
+          setDescription(e.target.value);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          fetch("http://localhost:3000/todo", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              title: title,
+              description: description,
+              completed: false,
+            }),
+          }).then(async function (response) {
+            const data = await response.json();
+            alert("todo created");
+          });
+        }}
+      >
+        Add a todo
+      </button>
     </div>
   );
 }
